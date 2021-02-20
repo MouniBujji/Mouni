feef370ec0c592aab07c76f53566a3b241fb5a5d
MouniBujji <77090056+MouniBujji@users.noreply.github.com>
2021-02-20 13:57:01 +0530

    Update Hello.java
    new commit

--- first example/src/main/java/Hello.java
+++ first example/src/main/java/Hello.java
@@ -2,6 +2,6 @@ class Hello
 {
    public static void main(String args[])
    {
-      System.out.println("Hello Welcome to Git Repository");
+      System.out.println("Hello Welcome to Git Repository....");
    }
 }
